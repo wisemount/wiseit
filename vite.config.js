1a84beb6507e66d36edaf3735ffb3528f810579c
sam2hub <ssamprabhu@gmail.com>
2025-07-12 20:28:28 +0530

    context root wiseit updated both with filesystem change

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/wiseit/' : '/'
-  
-  return {
-    base,
-    plugins: [react()],
-  }
+export default defineConfig({
+  base: '/wiseit/',
+  plugins: [react()],
 })
