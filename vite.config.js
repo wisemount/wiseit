98c3155de6128250a3cbe1cf2cdc4236960d073c
sam2hub <ssamprabhu@gmail.com>
2025-07-12 15:07:09 +0530

    Update vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wiseIT/', // Set this to your repo name for GitHub Pages
+  base: '/wiseit/', // Set this to your repo name for GitHub Pages
   plugins: [react()],
 })
