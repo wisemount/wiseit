1e861703fbbc2cda89d54415c338170a164b8b8e
sam2hub <ssamprabhu@gmail.com>
2025-07-12 23:54:12 +0530

    Complete vite config with build options

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
